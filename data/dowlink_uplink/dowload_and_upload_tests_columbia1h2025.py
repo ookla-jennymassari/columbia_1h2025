3f6e08b92384fea4a80fe34a6212aa0aa65e896a
Jenny Massari <jennymassari@ookla-mac-jennymassari.local>
2025-03-12 15:32:50 -0700

    Added download and upload folders.

---
+++ data/dowlink_uplink/dowload_and_upload_tests_columbia1h2025.py
@@ -0,0 +1,10 @@
+import pandas as pd
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+df_columbia_1h_2025 = pd.read_sql("select * from auto.vi_test_summary_unified_reporting tsr join md2.test_types tt using (test_type_id) where tsr.collection_set_id = 12698 and tsr.test_type_id in (19, 20);",
+                                      con=os.getenv('RSR_SVC_CONN'))
+
+print(df_columbia_1h_2025)
